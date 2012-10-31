4166095253136e3da1288dfd9b85f25ad587e63f
Pablo Karlsson <pablo.karlsson@gmail.com>
2012-10-31 23:09:28 +0100

    started new checkbox

--- gen/se/pablokarlsson/drawer/scare/R.java
+++ gen/se/pablokarlsson/drawer/scare/R.java
@@ -12,20 +12,23 @@ public final class R {
     }
     public static final class drawable {
         public static final int button_bg=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int pumpkin=0x7f020002;
-        public static final int seekbar_bg=0x7f020003;
+        public static final int check_box=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int pumpkin=0x7f020003;
+        public static final int seekbar_bg=0x7f020004;
     }
     public static final class id {
-        public static final int iDidNotGetScaredButton=0x7f080004;
-        public static final int iGotScaredButton=0x7f080003;
-        public static final int imageView1=0x7f080005;
-        public static final int menu_settings=0x7f080009;
-        public static final int post=0x7f080006;
-        public static final int shareButton=0x7f080007;
-        public static final int skipButton=0x7f080008;
-        public static final int startScareButton=0x7f080001;
+        public static final int cuseKnocking=0x7f080004;
+        public static final int iDidNotGetScaredButton=0x7f080006;
+        public static final int iGotScaredButton=0x7f080005;
+        public static final int imageView1=0x7f080007;
+        public static final int menu_settings=0x7f08000b;
+        public static final int post=0x7f080008;
+        public static final int shareButton=0x7f080009;
+        public static final int skipButton=0x7f08000a;
+        public static final int startScareButton=0x7f080003;
         public static final int textView1=0x7f080002;
+        public static final int textView2=0x7f080001;
         public static final int timeUntilScare=0x7f080000;
     }
     public static final class layout {
@@ -46,6 +49,7 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f050002;
+        public static final int maxTime=0x7f05000b;
         public static final int menu_settings=0x7f050000;
         public static final int no=0x7f050006;
         public static final int scared=0x7f050004;
@@ -55,6 +59,7 @@ public final class R {
         public static final int title_activity_main=0x7f050001;
         public static final int wanttoshare=0x7f050009;
         public static final int yes=0x7f050005;
+        public static final int zero=0x7f05000a;
     }
     public static final class style {
         public static final int AppTheme=0x7f060000;
